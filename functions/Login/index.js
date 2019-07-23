819b2a4c8b400d61c9fe7fc3fad61124d21c663c
hengsovandara <hengsovandara15@gmail.com>
2019-07-23 17:25:44 +0700

    modified login as more flexible

--- functions/Login/index.js
+++ functions/Login/index.js
@@ -3,42 +3,47 @@ const bcrypt  = require('bcrypt');
 const { loginResult, getRequestAct} = require('../../utils/helpers')
 
 module.exports = async ({ body = {} }) => {
-  const { phone, pin, tables, condition } = body
+  const { phone, pin, tables, condition, fields, needField } = body
 
   if (!!phone && !!pin)
-    return login(phone, pin, tables, condition);
+    return login(phone, pin, tables, condition, fields, needField);
 
   return loginResult(422, { 'Message': 'Miss matched parameters' })
 }
 
-async function login(phone, loginPin, tables = {}, condition = '') {
+async function login(phone, loginPin, tables = {}, condition = '', fields = `id pin`, needField = 'pin') {
 
   const table       = tables.credential && tables.credential || 'Credential'
-  condition         = `phoneNumber:{ _eq: "${phone}"} ${condition && condition || ''}`
+  condition         = condition && condition || `where: { phoneNumber:{ _eq: "${phone}"}}`
 
   const query = `
     query{
-      ${table}(where: {${condition}}){
-        id
-        pin
+      ${table}(${condition}){
+        ${fields}
       }
     }
   `
 
   const Credential = await getRequestAct('GQL', { query }).then( res => res[table])
 
-  if (Credential && Credential.length > 0){
-    const { pin, id } = Credential[0]
+  try{
+    if (Credential && Credential.length > 0){
+      let { id }  = Credential[0]
+      pin         = Credential[0][needField]
 
-    const check       = bcrypt.compareSync(loginPin, pin);
+      const check       = bcrypt.compareSync(loginPin, pin);
 
-    if (check){
-      token = await getToken(id, tables)
-      if(typeof token === 'string')
-        return loginResult(200, {token})
+      if (check){
+        token = await getToken(id, tables)
+        if(typeof token === 'string')
+          return loginResult(200, {token})
+      }
+      return loginResult(401, {Message:  `${needField} incorrect`})
     }
+    return loginResult(401, {Message: "Account does not exist."})
+  }catch(err){
+    return loginResult(401, {Message: "Unauthorized"})
   }
-  return loginResult(401, {Message: "Unauthorized"})
 }
 
 async function getToken(id, tables){
