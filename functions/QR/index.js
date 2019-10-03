62c23d4c0c5c932abd513396aad63209d6f607b1
hengsovandara <hengsovandara15@gmail.com>
2019-10-03 11:18:43 +0700

    update generate qr has png and pdf option

--- functions/QR/index.js
+++ functions/QR/index.js
@@ -6,14 +6,25 @@ module.exports = async (configs = {}) => {
   if(!configs)
     return fail("There is no configs to generatte QR.")
 
+  const { type = 'svg' } = configs
+
   try{
 
-    return svg(await toQr({ ...configs }));
+    switch (type) {
+      case 'svg':
+        return svg(await toSvgQr({ ...configs }));
+      case 'png':
+        return svg(await toPngQr(configs));
+      case 'pdf':
+        return svg(await toPdfQr(configs));
+      default:
+        return svg(await toSvgQr({ ...configs }));
+    }
 
   } catch(error){ return fail(error) }
 }
 
-function toQr(data = 'clik') {
+function toSvgQr(data = 'clik') {
   data = typeof data === 'object' ? JSON.stringify(data) : data;
 
   return new Promise((resolve, reject) => {
@@ -21,5 +32,25 @@ function toQr(data = 'clik') {
     const stream = qr.image(data, { type: 'svg' });
     stream.on('data', (buffer) => { string += buffer.toString() });
     stream.on('end', () => resolve('data:image/svg+xml;base64,' + Buffer.from(string).toString('base64')));
+  });
+}
+
+function toPngQr(data = clik) {
+  data = typeof data === 'object' ? JSON.stringify(data) : data;
+
+  return new Promise((resolve, reject) => {
+    const stream = qr.imageSync(data, { type: 'png', margin: 1, size: 20 });
+    resolve('data:image/png;base64,' + Buffer.from(stream).toString('base64'))
+  });
+}
+
+function toPdfQr(data = 'clik') {
+  data = typeof data === 'object' ? JSON.stringify(data) : data;
+
+  return new Promise((resolve, reject) => {
+    let string = '';
+    const stream = qr.image(data, { type: 'pdf' });
+    stream.on('data', (buffer) => { string += buffer.toString() });
+    stream.on('end', () => resolve('data:application/pdf;base64,' + Buffer.from(string).toString('base64')));
   });
 }
